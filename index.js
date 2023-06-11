01855e6628042854b60281d7f8021976ed591c98
Anne Edmondson <edmondson.anne@gmail.com>
2023-06-10 21:35:26 -0600

    Commit final html and js code

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-/* Using HTML, Bootstrap, and JavaScript create a single page website that contains the following:
-A Bootstrap styled table representing your choice of data.
-A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.*/
-
+function addBook() {
+    let bookTable =  document.querySelector('table')
+    let name = document.querySelector('form #name')
+    let bookTitle = document.querySelector('form #bookTitle')
+    let bookAuthor = document.querySelector('form #bookAuthor')
+    let bookRecommend = document.querySelector('form #bookRecommend')
+    let newBook = bookTable.innerHTML +=
+        `
+            <tr>
+                <td>${name.value}</td>
+                <td>${bookTitle.value}</td>
+                <td>${bookAuthor.value}</td>
+                <td>${bookRecommend.value}</td>
+            </tr>
+        `
+    form.reset()
+}
